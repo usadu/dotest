2790f7fbd51b96cf3ed37371980527bfd14af981
hcl <test@test.com>
2018-12-29 13:14:02 +0800

    Signed-off-by: hcl <test@test.com>

--- 第一阶段/src/复习/test.java
+++ 第一阶段/src/复习/test.java
@@ -14,7 +14,7 @@ public class test {
 			ArrayList<String> arr=new ArrayList<String>();
 			arr.add("a");
 			arr.add("b");
-			arr.add("cc");
+			arr.add("ccc");
 			arr.forEach(System.out::println);
 		
 	}
